4dec6e5a721b5c7e91b28c8df52bbe4e5d952bdc
Vishal Cherian <vishal@Vishals-MacBook-Pro.local>
2020-05-31 23:23:36 -0400

    switched to postgres from sqlite3

--- scratchbling/settings.py
+++ scratchbling/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'scratchbling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bytelion_scratchbling',
+        'USER': 'postgres',
+        'PASSWORD': 'testing123',
+        'HOST': 'bytelion-scratchbling.cpxhj3mpz7ot.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
