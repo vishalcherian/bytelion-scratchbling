cfbbfe05033cb3c0c8a59a6bef2127e76d7365c4
Vishal Cherian <vishal@Vishals-MacBook-Pro.local>
2020-06-01 00:04:58 -0400

    fixed config

--- scratchbling/settings.py
+++ scratchbling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#i@x6rpxqn@07ju7#bw-ua6hr-a(_4rnszj93n718o!xm=ynog'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-x37hcs6p.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
